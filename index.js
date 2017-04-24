2227b6441a78be5eedaf50fe2ec29211f37ba476
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-04-23 19:10:29 -0600

    chore (main) Made some WIP changes to files and deletes some

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000))
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/build'))
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
+app.get('*', function(request, response) {
+  response.sendFile(__dirname + '/build/index.html')
+})
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log("Express server started on port", app.get('port'))
+})
