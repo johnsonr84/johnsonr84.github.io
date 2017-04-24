563f654356e0ef569de559be422654041541cd91
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-04-23 18:20:08 -0600

    Demo

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var express = require('express')
-var app = express()
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/build'))
+app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(request, response) {
-  response.sendFile(__dirname + '/build/index.html')
-})
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Express server started on port", app.get('port'))
-})
+  console.log('Node app is running on port', app.get('port'));
+});
