1929299109ff9c72a76e91719efdf9e0f5bd0e2d
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-05-04 11:11:07 -0600

    test

--- src/screens/Screen1.js
+++ src/screens/Screen1.js
@@ -9,6 +9,7 @@ class Screen1 extends Component {
 
   render() {
     return (
+      <h1>Hello</h1>
       <div className="screen screen1">
         <img className="home-image" src={'./images/photo-white.png'} alt="rob" />
       </div>
